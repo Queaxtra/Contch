e86103be1456597edbb3c9269c32d021decbb783
Queaxtra <60826916+Queaxtra@users.noreply.github.com>
2022-08-12 02:41:57 +0300

    Hata düzeltmesi.

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,7 @@
 <div class="h-40">
     <footer class="footer footer-center p-4 bg-[#1A1A1A] text-base-content bottom-0 fixed mt-10">
         <div>
-            <p>© Cristaify - All right reserved <br> Made with by <a href="https://github.com/Queaxtra" class="text-orange-600">Queaxtra</a></p>
+            <p>© Contch - All right reserved <br> Made with by <a href="https://github.com/Queaxtra" class="text-orange-600">Queaxtra</a></p>
         </div>
     </footer>
 </div>
